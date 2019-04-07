ed5f2bcde4a651d2fb72c141e7756093e24da8af
Francesco Magno Reali <f.reali@outlook.com>
2019-04-08 01:15:48 +0200

    MVP publish

--- src/data/router/routes.js
+++
@@ -1,9 +0,0 @@
-import Homepage from '../../pages/Homepage.html';
-import Projects from '../../pages/Projects.html';
-import NotFound from '../../pages/NotFound.html';
-
-export const routes = {
-    homepage: Homepage,
-    hub: Projects,
-    notfound: NotFound
-}
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import App from './App.html';
-import { store } from './data/state-management/store';
-
-const app = new App({
-	target: document.body.querySelector('#app'),
-	store
-});
-
-export default app;
